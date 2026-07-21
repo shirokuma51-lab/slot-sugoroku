18fa67caf629dbe7ab219112fdbf8f144b4fd04f
shirokuma51-lab <nishiryo0501@gmail.com>
2026-07-21 14:21:55 +0900

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,9 +1,11 @@
 
-export const firebaseConfig = {
+export const firebaseConfig= {
   apiKey: "AIzaSyBynp7DQihyLsh3W2IeNS5dLbfDgF2S_94",
   authDomain: "slot-sugoroku.firebaseapp.com",
   projectId: "slot-sugoroku",
   storageBucket: "slot-sugoroku.firebasestorage.app",
-  messagingSenderId: "1:250192309861:web:2516180263fbeeae0aa08c",
+  messagingSenderId: "250192309861",
   appId: "1:250192309861:web:2516180263fbeeae0aa08c",
+  measurementId: "G-40M2EHCTNM"
 };
+
